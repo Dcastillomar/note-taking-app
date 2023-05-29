c0aeb1b7dd346152993de7efc99c219f0addcb60
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-05-29 09:13:00 -0500

    add routes.js file

--- Develop/public/sever.js
+++ Develop/public/sever.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html')) 
     );
